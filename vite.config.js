99d0e58a26225357e043a622e68618fecf0b418a
revosw <simenwiik@hotmail.com>
2025-07-21 13:06:43 +0200

    prevent notFound from triggering

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
+        devtoolsJson(),
 		tsconfigPaths(),
 		tailwindcss(),
 		// tailwindcss(), sentry(), ...
